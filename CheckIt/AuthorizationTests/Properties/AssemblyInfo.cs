385911636e48c86e9422deed06a739e0180abbe1
Kunal P <kunal1005@yahoo.com>
2019-03-15 18:27:31 -0700

    Fixed Solution and Logging Classes
    Builds properly, proper logging classes added

--- CheckIt/AuthorizationTests/Properties/AssemblyInfo.cs
+++ CheckIt/AuthorizationTests/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("AuthorizationTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,22 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("76b9e01b-7741-4795-86cd-d3024171ad80")]
+[assembly: Guid("2217b273-d608-42ce-b08c-526ab4a51336")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CheckIt/Logging/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public class Logger : ILog
+    {
+        string connectionString = string.Empty;
+        public override void TelLog(string message, Guid userID, DateTime login, DateTime logout, DateTime pageVisit, IPAddress userIP, string location)
+        {
+            lock (lockObj)
+            {
+                //Code to log data to the MongoDB
+            }
+        }
+    }
+}
