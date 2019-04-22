6c46d4585b3853070331d7f454521f8fded98576
Alex Philayvanh <aphilay@gmail.com>
2019-04-22 16:04:01 -0700

    Configured API controllers to return JSON vs XML

--- Checkit/WebApi-CheckIt/App_Start/WebApiConfig.cs
+++ Checkit/WebApi-CheckIt/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace WebApi_CheckIt
 {
@@ -19,6 +20,9 @@ namespace WebApi_CheckIt
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //return JSON instead of XML
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html") );
         }
     }
 }
