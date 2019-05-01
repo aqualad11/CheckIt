d49c2625f136f2ecf4c456f9eea5328fa8718d8e
Alex Philayvanh <aphilay@gmail.com>
2019-04-30 18:09:56 -0700

    Latest and greatest log project

---
+++ Checkit/Log/Log/LogRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Log
+{
+    public class LogRepository : ILogRepository
+    {
+        private StreamWriter logWriter;
+        //private StreamReader logReader;
+        private Config config = new Config();
+        private string directory;
+
+        public LogRepository()
+        {
+
+        }
+
+        public LogRepository(string fileName)
+        {
+
+            directory = config.GetLogDirectory();
+            logWriter = new StreamWriter(Path.Combine(directory, fileName), true);
+            //logReader = new StreamReader(Path.Combine(directory, fileName), true);
+           
+        }
+
+
+        /// <summary>
+        /// This method uses a StreamWriter object to write the log description to a file
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+           logWriter.WriteLine(message);
+           logWriter.Close();
+        }
+
+        /*
+        public string ReadTelemetryLog()
+        {
+            var jsonFile = logReader.ReadToEnd();
+            List<Telemetry> contents = JsonConvert.DeserializeObject<Telemetry>(jsonFile);
+            return jsonFile;
+        }
+
+     */
+
+    }
+}
---
+++ Checkit/Log/Log/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Log
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Config config = new Config();
+
+            string fileName = DateTime.UtcNow.ToString(config.GetDateTimeFormat()) + config.GetTelemetryLogExtention();
+            string tomorrow = "05-01-2019" + config.GetTelemetryLogExtension();
+
+            LogManager tlog = new LogManager(fileName);
+            LogManager tlog1 = new LogManager(tomorrow);
+            
+            //Telemetry Log Data
+            string uid = "35262";            
+            var info1 = new Telemetry
+            {
+                userID = uid,
+                dateTime = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss tt "),
+                description = "user has logged in"
+
+            };
+
+            tlog.LogTelemetry(info1);
+            tlog1.LogTelemetry(info1);
+
+
+
+
+
+            /*
+            try
+            {
+                divide(4, 0);
+            }
+            catch (Exception ex)
+            {
+
+                elogger.LogError(uid,ex);
+            }
+
+            */
+
+        }
+
+        public static int divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
