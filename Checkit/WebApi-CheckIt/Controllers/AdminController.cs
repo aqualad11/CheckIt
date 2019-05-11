aababa9fe0370629a4667ae162955a08ec156d6c
Unknown <kunal1005@yahoo.com>
2019-05-11 13:57:27 -0700

    Foundation for UAD backend

---
+++ Checkit/WebApi-CheckIt/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi_CheckIt.Controllers
+{
+    public class AdminController : ApiController
+    {
+        // GET api/User
+        [HttpGet]
+        [Route("api/uad/timeperpage")]
+        public List<int> Get()
+        {
+            return [1, 2, 3, 4, 5, 6];
+        }
+    }
+}
