e3b8123e2d43d87f8e3e036e4aca8cd3594dd724
Kunaledge <kunal1005@yahoo.com>
2019-05-15 00:53:42 -0700

    Update ILogRepository.cs

--- Checkit/DataAccessLayer/Repositories/ILogRepository.cs
+++ Checkit/DataAccessLayer/Repositories/ILogRepository.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,23 +16,3 @@ namespace CheckIt.DataAccessLayer.Repositories
         bool DeleteLog(string file);
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CheckIt.DataAccessLayer.Repositories
-{/// <summary>
-/// interface which declares methods for a log repository
-/// </summary>
-    public interface ILogRepository
-    {
-        bool LogTelemetry(string description);
-        bool LogError(string description);
-        List<string> GetLog(string file);
-        bool DeleteLog(string file);
-    }
-}
->>>>>>> master
