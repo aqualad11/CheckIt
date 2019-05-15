aed3683252d0861bca3715f860d24d49642473fa
Kunaledge <kunal1005@yahoo.com>
2019-05-15 00:56:43 -0700

    Update Config.cs

--- Checkit/ServiceLayer/Config.cs
+++ Checkit/ServiceLayer/Config.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace CheckIt.ServiceLayer
@@ -20,17 +20,11 @@ namespace CheckIt.ServiceLayer
             TEL_LOG_EXT = "_Telemetry.json";
             DATE_FORMAT = "yyyy-MM-dd";
             DATE_TIME_FORMAT = "MM/dd/yyyy hh:mm:ss tt ";
-<<<<<<< HEAD
-            LOG_DIRECTORY = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Logs";
-            LOG_BACKUP_DIRECTORY = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\LogsBackup";
-            LOG_ARCHIVE_DIRECTORY = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\LogArchives";
-            EMPTY_DIRECTORY = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\EmptyFolderOnPurpose";
-=======
+
             LOG_DIRECTORY = "C:\\Users\\Administrator\\Documents\\Logs"; //Directory.CreateDirectory(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName+"\\Logs").ToString();
             LOG_BACKUP_DIRECTORY = "C:\\Users\\Administrator\\Documents\\LogsBackup"; //Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\LogsBackup";
             LOG_ARCHIVE_DIRECTORY = "C:\\Users\\Administrator\\Documents\\LogArchives"; //Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\LogArchives";
             EMPTY_DIRECTORY = "C:\\Users\\Administrator\\Documents\\EmptyFolderOnPurpose"; //Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\EmptyFolderOnPurpose";
->>>>>>> master
         }
 
     }
